2c9e2fefa729a67c89d9365dbe1e628d092419bf
MAHAMDATEEQ <135177939+MAHAMDATEEQ@users.noreply.github.com>
2023-06-01 01:33:46 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+/**
+ * item - 0
+ * 
+ * def on button_pressed_a():
+ * 
+ *   global item
+ * 
+ *   item += 1
+ * 
+ * basic.show number (item)
+ * 
+ * input.on_button_pressed(Button.A, on_button_pressed_a)
+ * 
+ * def on button_pressed_b():
+ * 
+ * basic.show_string("mahamd ateeq!")
+ * 
+ * input.on_button_pressed(Button.B, on button pressed_b)
+ */
